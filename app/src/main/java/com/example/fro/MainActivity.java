f0b7b2b003c79026d51f52b6784e16bac76e2a77
giovanadanieles <giovana.daniele@usp.br>
2020-10-15 03:56:47 -0300

    Construída página inicial.

---
+++ app/src/main/java/com/example/fro/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
