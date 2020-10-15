8d3385b6ae484904cdd09b1ae5265e4906a89347
Bruna Magrini <magrinibruna@usp.br>
2020-10-15 14:04:23 -0300

    Adicionada tela de cadastro

--- app/src/main/java/com/example/fro/MainActivity.java
+++ app/src/main/java/com/example/fro/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.fro;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirTelaCadastro(View view){
+        Intent intent = new Intent(this, Cadastro.class);
+        startActivity(intent);
     }
 
 
